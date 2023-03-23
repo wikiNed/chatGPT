3ab5c971edeb7d9d90ed687eefe8f6db6134c9cb
KunWu <1007421637@qq.com>
2023-03-24 00:14:20 +0800

    feat（chartGPT基础）

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import './App.css';
 
+import { Cloud } from 'laf-client-sdk';
 import React, { useState } from 'react';
 
 import logo from './logo.svg';
 
 function App() {
   const [count, setCount] = useState(0);
+  // 创建 cloud 对象 这里需要将 <appid> 替换成自己的 App ID
+  const cloud = new Cloud({
+    baseUrl: 'https://smi590.laf.dev',
+    getAccessToken: () => '', // 这里不需要授权，先填空
+  });
+
+  async function send() {
+    /**
+     * todo 我们提问的内容
+     */
+    // const message = question.value;
+    //
+    // let res;
+    // // 与云函数逻辑一样，有上下文 id 就传入
+    // if (!parentMessageId.value) {
+    //     res = await cloud.invoke("send", {message});
+    // } else {
+    //     res = await cloud.invoke("send", {message, parentMessageId: parentMessageId.value});
+    // }
+    //
+    // // 回复我们的内容在 res.text
+    //
+    // // 这个是上下文 id
+    // parentMessageId.value = res.id;
+
+    const message = '你是谁';
+    const res = await cloud.invoke('send', { message });
+    console.log(res);
+  }
 
   return (
     <div className="App">
@@ -17,7 +47,12 @@ function App() {
         </p>
 
         <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
+          <button
+            onClick={
+              // () => setCount((count) => count + 1)
+              () => send()
+            }
+          >
             🪂 Click me : {count}
           </button>
 
